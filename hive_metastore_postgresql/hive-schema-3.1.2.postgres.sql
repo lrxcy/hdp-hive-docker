57aef8f6007ec6a81f19426fe686a1f38cc2e3f1
lrxcy <34975746+lrxcy@users.noreply.github.com>
2020-08-04 13:37:31 +0800

    Update and rename hive-schema-2.3.0.postgres.sql to hive-schema-3.1.2.postgres.sql

--- hive_metastore_postgresql/hive-schema-3.1.2.postgres.sql
+++ hive_metastore_postgresql/hive-schema-3.1.2.postgres.sql
@@ -1470,9 +1470,9 @@ GRANT ALL ON SCHEMA public TO PUBLIC;
 ------------------------------
 -- Transaction and lock tables
 ------------------------------
--- \i hive-txn-schema-2.3.0.postgres.sql;
+-- \i hive-txn-schema-3.1.2.postgres.sql;
 
 -- -----------------------------------------------------------------
 -- Record schema version. Should be the last step in the init script
 -- -----------------------------------------------------------------
-INSERT INTO "VERSION" ("VER_ID", "SCHEMA_VERSION", "VERSION_COMMENT") VALUES (1, '2.3.0', 'Hive release version 2.3.0');
+INSERT INTO "VERSION" ("VER_ID", "SCHEMA_VERSION", "VERSION_COMMENT") VALUES (1, '3.1.2', 'Hive release version 3.1.2');
