9b17a937f4aeb6e84723bc80e7fe1219cc654dbb
lrxcy <34975746+lrxcy@users.noreply.github.com>
2020-08-06 15:38:18 +0800

    Update and rename start-hadoop-spark-workbench-with-Hive.sh to start-hadoop-spark-workbench.sh

--- hdp-hive/start-hadoop-spark-workbench.sh
+++ hdp-hive/start-hadoop-spark-workbench.sh
@@ -6,8 +6,8 @@ docker-compose -f docker-compose-hive.yml up -d hive-server
 docker-compose -f docker-compose-hive.yml up -d spark-master spark-worker spark-notebook hue
 
 my_ip=`ip route get 1|awk '{print $NF;exit}'`
-echo "Namenode: http://${my_ip}:50070"
-echo "Datanode: http://${my_ip}:50075"
+echo "Namenode: http://${my_ip}:9870"
+echo "Datanode: http://${my_ip}:9864"
 echo "Spark-master: http://${my_ip}:8080"
 echo "Spark-notebook: http://${my_ip}:9001"
 echo "Hue (HDFS Filebrowser): http://${my_ip}:8088/home"
