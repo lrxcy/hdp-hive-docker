fff668bc1f7461bcce8623d893299be18c8f8884
lrxcy <34975746+lrxcy@users.noreply.github.com>
2020-08-07 14:59:34 +0800

    Update hive_user.sql

--- hive-metastore/scripts/hive_user.sql
+++ hive-metastore/scripts/hive_user.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE metastore;
 USE metastore;
-SOURCE $HIVE_HOME/scripts/metastore/upgrade/mysql/hive-schema-3.1.0.mysql.sql;
+SOURCE /opt/hive/scripts/metastore/upgrade/mysql/hive-schema-3.1.0.mysql.sql;
 CREATE USER 'hive'@'%' IDENTIFIED BY 'hive'; 
 GRANT all on *.* to 'hive'@localhost identified by 'hive';
 flush privileges;
