d8ab7a14af68c7efc09cf4ff11fbf41cd57c69b2
lrxcy <34975746+lrxcy@users.noreply.github.com>
2020-08-07 10:48:24 +0800

    Update entrypoint.sh

--- hive-metastore/scripts/entrypoint.sh
+++ hive-metastore/scripts/entrypoint.sh
@@ -4,5 +4,5 @@ export HADOOP_HOME=/opt/hadoop-3.2.1
 export HADOOP_CLASSPATH=${HADOOP_HOME}/share/hadoop/tools/lib/aws-java-sdk-bundle-1.11.375.jar:${HADOOP_HOME}/share/hadoop/tools/lib/hadoop-aws-3.2.1.jar
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
 
-/opt/apache-hive-metastore-3.0.0-bin/bin/schematool -initSchema -dbType mysql
-/opt/apache-hive-metastore-3.0.0-bin/bin/start-metastore
+/HIVE_HOME/bin/schematool -initSchema -dbType mysql
+/HIVE_HOME/bin/hive --service metastore
