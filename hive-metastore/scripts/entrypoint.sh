69de7927fc4b8cd33500230ac585cc4e2a9e1180
lrxcy <34975746+lrxcy@users.noreply.github.com>
2020-08-07 13:12:15 +0800

    Update entrypoint.sh

--- hive-metastore/scripts/entrypoint.sh
+++ hive-metastore/scripts/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+# start mysql
+service mysql start && mysql -u root -p < /usr/local/hive_user.sql "\n"
+
 ${HIVE_HOME}/bin/schematool -initSchema -dbType mysql
 ${HIVE_HOME}/bin/hive --service metastore
